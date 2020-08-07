b1096edc2842cccc2d9565d888fb9cd112e54d8a
bhidalto <borjabcn94@gmail.com>
2020-08-07 09:07:46 +0200

    Update main.py
    Updates and cleaning code

--- main.py
+++ main.py
@@ -1,50 +1,62 @@
 import os
-#Logging Libraries
+
+# Logging Libraries
 from google.cloud import logging
 from google.cloud.logging.resource import Resource
-#Tracing Libraries
+# Tracing Libraries
 from opencensus.trace import tracer as tracer_module
 from opencensus.trace.exporters import stackdriver_exporter
-from opencensus.trace.exporters.transports.background_thread \
-    import BackgroundThreadTransport
+from opencensus.trace.exporters.transports.background_thread import \
+    BackgroundThreadTransport
 
-#Fetch project id from the GCF env variables 
-PROJECT_ID=os.environ.get("GCP_PROJECT_ID")
- 
+# Fetch project id from the GCF env variables
+PROJECT_ID = os.environ.get("GCP_PROJECT")
+REGION = os.environ.get("FUNCTION_REGION")
+FUNC_NAME = os.environ.get("FUNCTION_NAME")
+TRACE = f"projects/{PROJECT_ID}/traces/382d4f4c6b7bb2f4a972559d9085001d"
+
 # instantiate trace exporter
 exporter = stackdriver_exporter.StackdriverExporter(
-project_id=PROJECT_ID, transport=BackgroundThreadTransport)
- 
-def hello_world(request):
-    #Create logging client
+    project_id=PROJECT_ID, transport=BackgroundThreadTransport)
+
+
+
+log_name = 'cloudfunctions.googleapis.com%2Fcloud-functions'
+
+test_dict = {"a": 1, "b": 2, "c": 3}
+# Inside the resource, nest the required labels specific to the resource type
+resource = Resource(type="cloud_function",  # must be cloud_function, in GKE put k8s_pod/k8s_container
+                    labels={
+                        "function_name": FUNC_NAME,
+                        "region": REGION
+                    })
+
+def gcf_trace_test(request):
+    # Create logging client
     log_client = logging.Client()
-    
-    #Stackdriver Tracer to be created, we will be passing the exporter and the sampler
-    tracer = tracer_module.Tracer(exporter=exporter, sampler=tracer_module.samplers.AlwaysOnSampler())
+    logger = log_client.logger(log_name.format(PROJECT_ID))
 
-    #Generic span, with nested spans to have better insights of the latency for each of the steps we want to execute
+    # Stackdriver Tracer to be created, we will be passing the exporter and the sampler
+    tracer = tracer_module.Tracer(
+        exporter=exporter, sampler=tracer_module.samplers.AlwaysOnSampler())
+
+    # Generic span, with nested spans to have better insights of the latency for each of the steps we want to execute
     with tracer.span(name='trace_function') as function_exec:
-        #Span 1, bold loop so we see a first step in GCF trace execution
-        with function_exec.span(name='nested_loop'):
-            for i in range(100):
-                pass
-        #Span 2, simple example on how to log a JSON to Stackdriver logging by using a Resource
-        with function_exec.span(name='nested_logs_preparation'):
-            # This is the resource type of the log
-            log_name = 'cloudfunctions.googleapis.com%2Fcloud-functions' 
-            test_dict = {"a": 1, "b" : 2, "c" : 3}
-            # Inside the resource, nest the required labels specific to the resource type
-            res = Resource(type="cloud_function", 
-                        labels={
-                            "function_name": "logging-levels", 
-                            "region": "europe-west1"
-                        },
-                        )
-            logger = log_client.logger(log_name.format(PROJECT_ID))
+        # Span 1
+        with function_exec.span(name='nested_loop') as span:
+            logger.log_struct(
+                test_dict, trace=TRACE, span_id=span.span_id, resource=resource, severity='WARNING')
 
-        #Span 3, outputting logs to Stackdriver, as a struct
-        with function_exec.span(name='logs_output'):    
-            logger.log_struct(test_dict, resource=res, severity='ERROR')
-    
+        # Span 2
+        with function_exec.span(name='nested_logs_preparation'):
+            logger.log_struct(
+                {"foo": 5}, trace=TRACE, span_id=span.span_id, resource=resource, severity='DEBUG')
+
+        # Span 3
+        with function_exec.span(name='logs_output') as span:
+            logger.log_struct(test_dict, trace=TRACE, span_id=span.span_id,
+                              resource=resource, severity='ERROR')
+
     # We return a string as a result of our GCF execution
-    return 'Wrote logs to {}.'.format(logger.name) # Return cloud function response
+    # Return cloud function response
+    return 'Wrote logs to {}.'.format(logger.name)
